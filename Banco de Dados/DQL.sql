e8190592801030ed15ba6d39e6d1c5dbc31b67ae
FIC <fic@info.local>
2025-09-01 21:47:22 -0300

    Add DQL

---
+++ Banco de Dados/DQL.sql
@@ -0,0 +1,74 @@
+-- DQL 
+-- PRINCIPAL COMANDO DQL E O SELECT 
+
+
+SELECT nome_completo, telefone FROM ecommerce.cliente;
+
+SELECT nome, idade, cpf FROM clinica.paciente;
+
+SELECT * FROM ecommerce.produto;
+
+SELECT * FROM clinica.consulta;
+
+--JOIN 
+-- Com o JOIN posso juntar tabelas
+-- Utilizando a chave estrangeira
+
+
+-- Listar, data , Valor , Nome medico
+
+SELECT 
+	cs.data,
+	valor,
+	nome
+FROM clinica.consulta AS cs     -- Atribui um apelido para diferenciar data de consulta e data de medico caso 
+--tenha mais de uma coluna com o mesmo nome
+JOIN
+	clinica.medico AS med
+ON  clinica.consulta.id_medico = clinica.medico.id_medico;
+
+
+SELECT 
+	cs.data,
+	valor,
+	nome
+FROM clinica.consulta AS cs     -- Atribui um apelido para diferenciar data de consulta e data de medico caso 
+--tenha mais de uma coluna com o mesmo nome
+JOIN
+	clinica.medico AS med
+ON  cs.id_medico = clinica.medico.id_medico;
+
+-- Exercicio 1
+SELECT 
+	pedido_id,
+	nome_completo
+FROM ecommerce.pedido
+JOIN ecommerce.cliente
+ON	ecommerce.cliente.cliente_id = ecommerce.cliente.cliente_id;
+
+SELECT * FROM ecommerce.pedido;
+
+
+-- Exercicio 1
+SELECT nome_completo, email FROM ecommerce.cliente;
+
+-- Exercicio 2
+SELECT * FROM ecommerce.produto
+SELECT * FROM ecommerce.produto where preco > 450.00;
+
+-- Exercicio 3
+
+INSERT INTO ecommerce.cliente(nome_completo, email, senha, telefone, data_cadastro) VALUES
+('Joao Silva', 'edipocb12@email.com', 'Teste@123', 13963245544, '2025-08-28')
+SELECT * FROM ecommerce.cliente WHERE telefone like '%11%';
+
+-- Exercicio 4 
+INSERT INTO ecommerce.produto(nome_produto, descricao, preco, estoque_disponivel, imagem_url) VALUES
+('Meiao', 'Jogo', '40', 55, 'Teste_email')
+
+
+
+
+
+
+
